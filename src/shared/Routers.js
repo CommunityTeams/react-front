1efb56d4771957a7db954c0a9a61b7f289c26c25
SeungwooYang <dg6800000@gmail.com>
2022-12-12 01:21:43 -0800

    Update Routers.js
    자동완성으로 ''대신 ""로 수정됨,
    PostAdd 글쓰기 페이지 url로 링크 추가,
    홈으로 돌아올수 있는 Header 컴포넌트 리턴에 추가 - 승우

--- src/shared/Routers.js
+++ src/shared/Routers.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Home from '../components/features/Home';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../components/features/Home";
+import { Header } from "../components/layout/Header_sw";
+import PostAdd from "../components/features/PostAdd";
 
 export default function Homepage() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/Post/add" element={<PostAdd />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }

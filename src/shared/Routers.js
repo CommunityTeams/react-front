1ebadfaa84a27045e6e94901f99a03236eaa7e49
SeungwooYang <dg6800000@gmail.com>
2022-12-13 12:43:15 -0800

    Update Routers.js
    헤더 경로 삭제

--- src/shared/Routers.js
+++ src/shared/Routers.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../components/features/Home";
-import { Header } from "../components/layout/Header_sw";
-import PostAdd from "../components/features/PostAdd";
+import Home from "../components/layout/Home";
+import PostAdd from "../components/pages/PostAdd";
 
 export default function Homepage() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
         <Route path="/Post/add" element={<PostAdd />} />
       </Routes>
     </BrowserRouter>

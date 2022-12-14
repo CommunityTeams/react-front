5634958d5a5d470737da26212e8efefd540cfefb
SeungwooYang <dg6800000@gmail.com>
2022-12-14 11:08:36 -0800

    Update PostAdd.jsx
    can't make submitting feature...

--- src/components/pages/PostAdd.jsx
+++ src/components/pages/PostAdd.jsx
@@ -1,33 +1,67 @@
 import React from "react";
-// import { useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useState , useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 // import {v4 as uuidv4} from "uuid";
 import styled from "styled-components";
-// import { addPost, __getComment } from '../redux/modules/postSlice';
-// import { __addComment } from "../redux/modules/postSlice";
+import axios from "axios";
+import {__addNewPost } from '../../redux/modules/postsSlice';
+// import {__addNewPost, __updatePost} from '../../redux/modules/postsSlice';
+import newPost from '../pages/newPost';
 
 function PostAdd() {
+  const { postId } = useParams();
+  const postText = useSelector((state) => state.postText);
   // dispatch
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   // useState
-
-  // onSubmitHandler
-
+  const [list] = useState({
+    id: "",
+    title: "",
+    content:"",
+  });
+  const [lists, setLists] = useState({
+    id: 0,
+    title: "",
+    content:"",
+  });
+  const fetchLists = async () => {
+    const { data } = await axios.get("http://localhost:3004/lists");
+    setLists(data);
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    dispatch(__addNewPost(newPost(postText)));
+    navigate('/');
+    
+  };
+  // const onSubmitHandler = (list) => {
+  //   axios.post("http://localhost:3004/lists", list);
+  //   setLists([...lists, { ...list, id: lists.length + 1 }]);
+  // };
   // onChangeHandler
+  // const onChangeHandler = (event) => {
+  //   event.preventDefault();
+  //   dispatch(__updatePost({ ...postText, id: postId }));
+  //   navigate(`/details/${postId}`);
+  // };
+  //useEffect
+  useEffect(() => {
+    fetchLists();
+  }, []);
 
-  // __addComment 시간나면 댓글달기
-
-  // __getComment 시간나면
-
-  return (
+return (
     <StContainer>
-      {/* <StAddForm action="" onSubmit={onSubmitHandler}> */}
-      <StAddForm action="">
+      <StAddForm action="" onSubmit={(e) => {
+          e.preventDefault();
+          onSubmitHandler(list);
+        }}>
         <h2>작성자</h2>
         <StInputGroup
           type="text"
-          name="user"
-          // value={posts.user}
+          // name="user"
+          // value={lists.user}
           // onChange={onChangeHandler}
           placeholder="이름을 입력해주세요"
         />
@@ -35,7 +69,7 @@ function PostAdd() {
         <StInputGroup
           type="text"
           name="title"
-          // value={posts.title}
+          value={lists.title}
           // onChange={onChangeHandler}
           placeholder="제목을 입력해주세요"
         />
@@ -43,11 +77,11 @@ function PostAdd() {
         <Textarea
           type="text"
           name="content"
-          // value={posts.content}
+          value={lists.content}
           // onChange={onChangeHandler}
           placeholder="내용을 입력해주세요"
         />
-        <button type="button" size="large">
+        <button type="submit" size="large">
           추가하기
         </button>
       </StAddForm>

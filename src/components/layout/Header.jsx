f7ac5a1fce58aa99e12e121e8848e9a04f3a1c0c
minseung-gang <strause1@nate.com>
2022-12-15 13:23:22 +0900

    Design: 헤더 css 수정

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,35 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-let Div = styled.div`
+const Div = styled.div`
   width: 100%;
-  height: 30px;
-  margin: auto;
+  padding: 5px;
+  box-sizing: border-box;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
   display: flex;
   justify-content: space-between;
   border-bottom: 1px solid silver;
+  align-items: center;
+  border-bottom: 1px solid #ececec;
 `;
 
-let Img = styled.img`
+const Img = styled.img`
   width: 30px;
   height: 30px;
 `;
-let H3 = styled.h3`
-  width: 160px;
-  margin-top: 5px;
+const H3 = styled.h3`
+  width: fit-content;
+  font-family: "Noto Sans KR", sans-serif;
 `;
 
+// eslint-disable-next-line import/prefer-default-export
 export function Header() {
   return (
-    <>
-      <div>
-        <Div>
-          <Link to={"/"} title="집 아이콘">
-            <Img src="https://cdn-icons-png.flaticon.com/512/60/60817.png" />
-          </Link>
-          <H3>🛳️10기 B반 전용 게시판</H3>
-        </Div>
-      </div>
-    </>
+    <div>
+      <Div>
+        <Link to="/" title="집 아이콘">
+          <Img src="https://cdn-icons-png.flaticon.com/512/60/60817.png" />
+        </Link>
+        <H3>🛳️ 10기 B반 전용 게시판</H3>
+      </Div>
+    </div>
   );
 }
